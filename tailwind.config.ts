cc6e2435f0f0b6458d88223f26d9f80e389e14bf
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-07-30 23:41:29 +0900

    feat: implement Main Responsive UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        "2xs": { min: "276px", max: "359px" }, // Fold
+        xs: { min: "360px", max: "389px" }, // Mobile
+        "2sm": { min: "390px", max: "559px" }, // Wide Mobile
+        sm: { min: "560px", max: "667px" }, // Wide Wide Mobile - 500px
+        tb: { min: "668px", max: "1023px" }, // Tablet
+        lg: { min: "1024px" }, // Laptop
+        xl: { min: "1280px" }, // Desktop
       },
       fontFamily: {
         brush: ["Nanum Brush Script", "cursive"],
